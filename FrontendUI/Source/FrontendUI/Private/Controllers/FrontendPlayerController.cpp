0b3419a50af9eae789e6b47de0a153c709bcddbe
DESKTOP-94M8TSN\wing9 <wing997@naver.com>
2026-05-11 00:28:07 +0900

    Set View Target

--- FrontendUI/Source/FrontendUI/Private/Controllers/FrontendPlayerController.cpp
+++ FrontendUI/Source/FrontendUI/Private/Controllers/FrontendPlayerController.cpp
@@ -1,5 +1,19 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 
 #include "Controllers/FrontendPlayerController.h"
 
+#include "Camera/CameraActor.h"
+#include "Kismet/GameplayStatics.h"
+
+void AFrontendPlayerController::OnPossess(APawn* aPawn)
+{
+	Super::OnPossess(aPawn);
+
+	// Searching Camera
+	TArray<AActor*> FoundCameras;
+	UGameplayStatics::GetAllActorsOfClassWithTag(this, ACameraActor::StaticClass(), FName("Default"),FoundCameras);
+
+	if (!FoundCameras.IsEmpty())
+	{
+		SetViewTarget(*FoundCameras.begin());
+	}
+}
--- FrontendUI/Source/FrontendUI/Public/Controllers/FrontendPlayerController.h
+++ FrontendUI/Source/FrontendUI/Public/Controllers/FrontendPlayerController.h
@@ -8,5 +8,10 @@ UCLASS()
 class FRONTENDUI_API AFrontendPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+protected:
+	// >> Begin APlayerController Interface
+	virtual void OnPossess(APawn* aPawn) override;
+	// << End APlayerController Interface
 	
 };
