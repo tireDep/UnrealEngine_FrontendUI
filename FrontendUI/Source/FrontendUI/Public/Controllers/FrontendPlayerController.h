e8947f35bee4f7d3383f1d29bcec2a12b535551b
DESKTOP-94M8TSN\wing9 <wing997@naver.com>
2026-05-10 23:50:32 +0900

    기본 파일 추가

---
+++ FrontendUI/Source/FrontendUI/Public/Controllers/FrontendPlayerController.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FrontendPlayerController.generated.h"
+
+UCLASS()
+class FRONTENDUI_API AFrontendPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
