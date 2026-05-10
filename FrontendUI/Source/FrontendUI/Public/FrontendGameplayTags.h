d4e2be9893eb0a6594b074b82df2cf657110d1ee
DESKTOP-94M8TSN\wing9 <wing997@naver.com>
2026-05-11 00:44:59 +0900

    Widget Stack Gameplay Tags

---
+++ FrontendUI/Source/FrontendUI/Public/FrontendGameplayTags.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace  FrontendGameplayTags
+{
+	FRONTENDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Modal);
+	FRONTENDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameMenu);
+	FRONTENDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameHud);
+	FRONTENDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Frontend);
+}
